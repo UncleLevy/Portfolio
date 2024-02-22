c745d3517b0436d0b4e6c5442727e585eb8b309e
UncleLevy <54694851+UncleLevy@users.noreply.github.com>
2024-02-22 16:36:04 +0200

    Eleventh Commit

--- script.js
+++ script.js
@@ -15,26 +15,6 @@ hiddenElements.forEach((el) => {
     observer.observe(el);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('contactForm');
-
-    form.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevent the default form submission
-
-        const formData = new FormData(form);
-
-        fetch('sendmail.php', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.text())
-            .then(data => {
-                console.log('Success:', data);
-                // Handle success, e.g., show a success message
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                // Handle error, e.g., show an error message
-            });
-    });
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.nav-links').classList.toggle('show');
 });
